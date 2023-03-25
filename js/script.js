d321ad7404e2f52a125f60919ac0212cf03637ff
NicolasGonzalez0418 <127908023+NicolasGonzalez0418@users.noreply.github.com>
2023-03-25 11:54:53 -0500

    V3
    version 3
    
    Signed-off-by: NicolasGonzalez0418 <127908023+NicolasGonzalez0418@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -4,5 +4,8 @@ function mostrar_menu(){
 
      document.getElementById("move-content").classList.toggle('move-container-all');
      document.getElementById("show-menu").classList.toggle('show-lateral');
-
 }
+/*INGRESAR*/
+
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");});
